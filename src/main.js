6d395f6b093c526f7838b2909bffb2b5e6a0c445
neha doodi <nehadoodipoonia@gmail.com>
2020-02-07 16:28:39 -0800

    update user interface

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import $ from 'jquery';
-import 'bootstrap' ; 
-import 'bootstrap/dist/css/bootstrap.min.css' ; 
-import './styles.css' ;
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import './years';
+import { Years } from './years';
+
+$(document).ready(function () {
+  $("#ageCalculator").submit(function (event) {
+    event.preventDefault();
+    $('.result').$; { Years, calculateMercuryYears() };
+
+
+
+
+
+
+
+
+
+
+
+  });
+});
